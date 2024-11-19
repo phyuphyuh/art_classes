c7b0b82b2126d206bada43a20144af4163947cc3
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-11-19 11:28:15 +0900

    added empty controllers and views

---
+++ app/controllers/art_classes_controller.rb
@@ -0,0 +1,16 @@
+class ArtClassesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/art_classes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ArtClassesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
