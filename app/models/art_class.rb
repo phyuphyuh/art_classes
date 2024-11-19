eb280503d895fdbdb62244b78d094e0bfa4e6941
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-11-19 11:02:48 +0900

    added controllers

--- app/models/art_class.rb
+++ app/models/art_class.rb
@@ -1,4 +1,12 @@
 class ArtClass < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :price, presence: true
+  validates :location, presence: true
+  validates :dates, presence: true
+  CATEGORIES = ["Pottery", "Oil Painting", "Watercolor", "Acrylic", "Still Life", "Sketching", "Manga", "Sculpture", "DIY", "Other"]
+  validates :category, inclusion: { in: CATEGORIES }
+  has_one_attached :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: 'pages#home'
+  resources :art_classes, only: [:index, :show, :new, :create, :destroy] do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations, only: [:index, :update, :destroy]
 end
