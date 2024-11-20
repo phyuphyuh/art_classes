9bfbd09a7754af87e4b8cde9ed67f8aad5a46f96
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-11-20 15:31:36 +0900

    removed users controller

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :art_class
   belongs_to :user
+  validates :user, uniqueness: { scope: :art_class }
   enum status: [ :pending, :accepted, :rejected ]
 end
