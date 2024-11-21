ad90aa825dde63a4e0f1f34091acba86ed4f035d
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2024-11-21 10:43:36 +0900

    few small updates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :art_class
   belongs_to :user
-  # validates :user, uniqueness: { scope: :art_class }
   enum status: [ :pending, :accepted, :rejected ]
 end
