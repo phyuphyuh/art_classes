8b2efaee4f88f8b380f27bea63e2f05d978ff145
mason kamolpechara <mason.kamolpechara@gmail.com>
2024-11-21 12:47:49 +0900

    added approval to cards

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :art_class
   belongs_to :user
-  enum status: [ :pending, :accepted, :rejected ]
+  enum status: { pending: "pending", accepted: "accepted", rejected: "rejected" }
 end
